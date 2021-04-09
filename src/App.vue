b80bbafcbc72f16afe2975d9f9cbfc658ebc58fb
redcodemohammed <redcodemohammed@gmail.com>
2021-04-09 14:19:45 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <h2 class="m-2">Hello</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+<style src="@/assets/tailwindcss.css">
